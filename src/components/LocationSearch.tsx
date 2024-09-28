c7c900de6d3320983baff8e26f45d38231a40447
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-09-28 11:05:45 +0100

    Update component to render error message if location is not found

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {Fragment, useState} from 'react';
 import type {Place} from '../api/Place';
 import {search} from '../api/search';
@@ -9,11 +10,18 @@ interface LocationSearchProps {
 export default function LocationSearch({onPlaceClick}: LocationSearchProps) {
     const [term, setTerm] = useState('');
     const [places, setPlaces] = useState<Place[]>([]);
+    const [error, setError] = useState<{error: string}>({error: ''});
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const results = await search(term);
-        setPlaces(results);
+        const results: Place[] | {error: string} = await search(term);
+        if (Array.isArray(results)) {
+            setPlaces(results);
+            setError({error: ''});
+        } else if (!Array.isArray(results)) {
+            setPlaces([]);
+            setError(results);
+        }
         setTerm('');
     };
 
@@ -28,26 +36,30 @@ export default function LocationSearch({onPlaceClick}: LocationSearchProps) {
                     id="term"
                     value={term}
                     onChange={(e) => setTerm(e.target.value)}
-                    placeholder="Enter location name"
+                    placeholder="Enter location address"
                 />
             </form>
 
             <h1 className="font-bold mt-6">Found Locations</h1>
             <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
-                {places.map((place) => {
-                    return (
-                        <Fragment key={place.id}>
-                            <p className="text-sm">{place.name}</p>
-                            <button
-                                type="button"
-                                className="bg-blue-500 text-xs text-white font-bold py-1 rounded"
-                                onClick={() => onPlaceClick(place)}>
-                                Go
-                            </button>
-                            <div className="border-b w-full col-span-2" />
-                        </Fragment>
-                    );
-                })}
+                {places.length > 0 &&
+                    places.map((place) => {
+                        return (
+                            <Fragment key={place.id}>
+                                <p className="text-sm">{place.name}</p>
+                                <button
+                                    type="button"
+                                    className="bg-blue-500 text-xs text-white font-bold py-1 rounded"
+                                    onClick={() => onPlaceClick(place)}>
+                                    Go
+                                </button>
+                                <div className="border-b w-full col-span-2" />
+                            </Fragment>
+                        );
+                    })}
+                {places.length === 0 && error && (
+                    <p className="text-sm">{error.error}</p>
+                )}
             </div>
         </div>
     );
